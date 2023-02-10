7774e28ccb47fae301940c7a5d135dd5a923ab23
HazelDaniel <hazeldaniel856@gmail.com>
2023-02-10 15:13:24 -0600

    checking sizes of types and their limits on my machine _cont1

--- main.c
+++ main.c
@@ -4,12 +4,13 @@
 
 int main()
 {
-    printf("size of INT on this machine is : %ld ", sizeof(int));
-    printf("size of FLOAT on this machine is : %ld ", sizeof(float));
-    printf("size of DOUBLE on this machine is : %ld ", sizeof(double));
-    printf("size of LONG DOUBLE on this machine is : %ld ", sizeof(long double));
-    printf("size of LONG LONG on this machine is : %ld ", sizeof(long long));
-    printf("size of SHORT on this machine is : %ld ", sizeof(short));
+    printf("size of INT on this machine is : %lu ", sizeof(int));
+    printf("size of FLOAT on this machine is : %lu ", sizeof(float));
+    printf("size of DOUBLE on this machine is : %lu ", sizeof(double));
+    printf("size of LONG on this machine is : %lu ", sizeof(long));
+    printf("size of LONG DOUBLE on this machine is : %lu ", sizeof(long double));
+    printf("size of LONG LONG on this machine is : %lu ", sizeof(long long));
+    printf("size of SHORT on this machine is : %lu ", sizeof(short));
     printf("===============================================\n");
     printf("the max value of an INT on this machine is : %d ", INT_MAX);
     printf("\n\t unsigned : %u \n", UINT_MAX);
@@ -23,6 +24,8 @@ int main()
     printf("\n\t unsigned : %u \n", 0);
     printf("the max value of a LONG on this machine is : %ld ", LONG_MAX);
     printf("\n\t unsigned : %lu \n", ULONG_MAX);
+    printf("the max size of a LONG LONG INT on this machine is : %Ld ",LLONG_MAX);
+    printf("\n\t unsigned : %Lu ", ULLONG_MAX);
 
     return 0;
 }
